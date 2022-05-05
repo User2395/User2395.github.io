3411fbbb2ca353b6d85c8ead4cd72f7bead5fc48
Stefanos Das <52162383+User2395@users.noreply.github.com>
2022-05-05 11:22:07 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function setup() {
 function draw() {
   background(0);
   textSize(42);
-  image(gif, windowWidth-gif.width * 1.2,500,gif.width,gif.height);
+  image(gif, windowWidth-gif.width * 1.2,400,gif.width,gif.height);
   gif.play();
 
   now = new Date().getTime();
@@ -32,13 +32,13 @@ function draw() {
 
 
   fill(255);
-  text("Athens 2022",windowWidth/2.2,windowHeight/1.5);
+  text("Athens 2022",windowWidth/2.2,windowHeight/1.6);
 
   fill(128,255,128);
-  text(`${days}:${hours}:${minutes}:${seconds}`,windowWidth/2.2,windowHeight/1.35);
+  text(`${days}:${hours}:${minutes}:${seconds}`,windowWidth/2.2,windowHeight/1.45);
 
   textSize(14);
-  text(`DAYS  HOURS  MINUTES SECONDS`,windowWidth/2.19,windowHeight/1.3);
+  text(`DAYS  HOURS  MINUTES SECONDS`,windowWidth/2.19,windowHeight/1.4);
 
 	image(img,windowWidth/2.6,windowHeight/4,img.width/2,img.height/2);
 }
